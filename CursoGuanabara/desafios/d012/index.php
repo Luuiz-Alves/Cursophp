16682e45f508019e43d6987016d329d563f22fa0
Luiz Alves <98660064+Luuiz-Alves@users.noreply.github.com>
2023-06-01 20:53:31 -0300

    terminei o desafio 012

--- CursoGuanabara/desafios/d012/index.php
+++ CursoGuanabara/desafios/d012/index.php
@@ -15,22 +15,36 @@
         <h1>Calculadora de Tempo</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="segundos">Qual é o total de segundos?</label>
-            <input type="number" name="segundos" id="segundos">
+            <input type="number" name="segundos" id="segundos" min="0" step="1" required value="<?=$segundos?>">
             <input type="submit" value="Calcular">
         </form>
     </main>
     <?php 
-    
+        $sobra = $segundos;
+        // Total de Semanas
+        $semana = (int)($sobra / 604_800);
+        $sobra = $sobra % 604_800;
+        // Total de Dias
+        $dia = (int)($sobra / 86_400);
+        $sobra = $sobra % 86_400;
+        // Total de Horas
+        $hora = (int)($sobra / 3_600);
+        $sobra = $sobra % 3_600;
+        // Total de Minutos
+        $minuto = (int)($sobra / 60);
+        $sobra = $sobra % 60;
+        // Total de Segundos
+        $total_segundos = $sobra;
     ?>
     <section>
         <h2>Totalizando tudo</h2>
-        <p>Analisando o valor que você digitou, <strong><?=$segundos?> segundos</strong> equivalem a um total de:</p>
+        <p>Analisando o valor que você digitou, <strong><?=number_format($segundos, 0, ",", ".")?> segundos</strong> equivalem a um total de:</p>
         <ul>
-            <li>{} semanas</li>
-            <li>{} dias</li>
-            <li>{} horas</li>
-            <li>{} minutos</li>
-            <li>{} segundos</li>
+            <li><?=$semana?> semanas</li>
+            <li><?=$dia?> dias</li>
+            <li><?=$hora?> horas</li>
+            <li><?=$minuto?> minutos</li>
+            <li><?=$total_segundos?> segundos</li>
         </ul>
     </section>
 </body>
